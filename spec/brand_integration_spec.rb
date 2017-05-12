4bb83679e8268d735c087ce265fb31195f27a5f9
Asia Kane <asialkane@gmail.com>
2017-05-12 15:15:44 -0700

    add UI test to check the brand update path

--- spec/brand_integration_spec.rb
+++ spec/brand_integration_spec.rb
@@ -11,3 +11,16 @@ describe("the brand creation path", {:type => :feature}) do
     expect(page).to have_content('Adidas')
   end
 end
+
+describe("the brand update", {:type => :feature}) do
+  it("starts at the homepage and creates a new brand") do
+    brand = Brand.create({brandname: 'lulu'})
+    visit("/brands")
+    click_link('Lulu')
+    expect(page).to have_content('Update the Lulu Brand')
+    fill_in('brandname', :with => 'Lululemon')
+    fill_in('price', :with => 100)
+    click_button('Update Brand')
+    expect(page).to have_content('Lululemon')
+  end
+end
