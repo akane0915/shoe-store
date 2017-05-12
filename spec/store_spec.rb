54b690a44279e06d5f37d1a33c40da74af28a222
Asia Kane <asialkane@gmail.com>
2017-05-12 09:52:40 -0700

    add long-hand spec to return the brands for a given store when calling store.brands

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -3,6 +3,22 @@ require "spec_helper"
 describe(Store) do
   it {should have_and_belong_to_many :brands}
 
+
+    it "returns the store's storename" do
+      store = Store.create({storename: "Portland"})
+      brand = Brand.create({brandname: "Adidas"})
+      store.brands.push(brand)
+      expect(store.brands). to eq [brand]
+    end
+
+
+  describe "#storename" do
+    it "returns the store's storename" do
+      store = Store.create({storename: "Portland"})
+      expect(store.storename). to eq "Portland"
+    end
+  end
+
   describe "#capitalize_storename" do
     it "converts the storename first letter to a capital letter before saving" do
       store = Store.create({storename: "portland"})
