a1b02804aaf359c369c0374348926aa628577a48
Asia Kane <asialkane@gmail.com>
2017-05-12 09:47:04 -0700

    add passing tests to return brandname and brand price as well as method called currency_format to return the price of a brand in currency format

--- lib/brand.rb
+++ lib/brand.rb
@@ -2,8 +2,15 @@ class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
   before_save :capitalize_brandname
 
+  def currency_format
+    currency_price = price.to_s
+    "$#{currency_price}0"
+  end
+
   private
     def capitalize_brandname
       brandname.capitalize!
     end
+
+
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -3,10 +3,31 @@ require "spec_helper"
 describe(Brand) do
   it {should have_and_belong_to_many :stores}
 
+  describe "#brandname" do
+    it "returns the brand's brandname" do
+      brand = Brand.create({brandname: "Adidas"})
+      expect(brand.brandname). to eq "Adidas"
+    end
+  end
+
+  describe "#price" do
+    it "returns the brand's price" do
+      brand = Brand.create({brandname: "Adidas", price: 20})
+      expect(brand.price). to eq 20.0
+    end
+  end
+
   describe "#capitalize_brandname" do
     it "converts the brandname first letter to a capital letter before saving" do
       brand = Brand.create({brandname: "adidas"})
       expect(brand.brandname). to eq "Adidas"
+    end
+  end
+
+  describe "#currency_format" do
+    it "is called on the brand instance and returns the price of the brand in currency format with 2 characters after the decimal point" do
+      brand = Brand.create({brandname: "adidas", price: 20})
+      expect(brand.currency_format). to eq "$20.00"
     end
   end
 end
